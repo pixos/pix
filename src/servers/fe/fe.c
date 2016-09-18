082d131bb13d07f54be9728f7672044b040355f3
Hirochika Asai <panda@jar.jp>
2016-09-19 02:40:12 +0900

    Implementing the forwarding engine in user-land.

---
+++ src/servers/fe/fe.c
@@ -0,0 +1,69 @@
+/*_
+ * Copyright (c) 2016 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
+#include "pci.h"
+
+void
+sysxpsleep(void)
+{
+    __asm__ __volatile__ ("syscall" :: "a"(SYS_xpsleep));
+}
+
+/*
+ * Entry point for the process manager program
+ */
+int
+main(int argc, char *argv[])
+{
+    char buf[512];
+    int ret;
+    uint8_t *mem;
+
+    /* Map */
+    mem = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1,
+               0);
+    strcpy(mem, "abcd efgh");
+
+    while ( 1 ) {
+        ret = pci_read_config(0, 0, 0, 0);
+        snprintf(buf, 512, "xxx %s %x %x %s", "abcd", ret, mem, mem);
+        write(1, buf, strlen(buf));
+        sysxpsleep();
+    }
+    exit(0);
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
