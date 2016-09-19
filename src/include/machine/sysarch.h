87378bff17f636daed7cb41906e596d1d83508b5
Hirochika Asai <panda@jar.jp>
2016-09-19 23:14:40 +0900

    Added sysarch.h

---
+++ src/include/machine/sysarch.h
@@ -0,0 +1,50 @@
+/*_
+ * Copyright (c) 2015 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _MACHINE_SYSARCH_H
+#define _MACHINE_SYSARCH_H
+
+#define SYSARCH_INL     1
+#define SYSARCH_OUTL    2
+#define SYSARCH_CPUMAP  11
+
+struct sysarch_io {
+    long long port;
+    long long data;
+};
+struct sysarch_cpumap {
+    int id;
+};
+
+int sysarch(int, void *);
+
+#endif /* _MACHINE_SYSARCH_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
