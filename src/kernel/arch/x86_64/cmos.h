0531265a54d65d881345b56bb1a28be021799d72
Hirochika Asai <panda@jar.jp>
2016-11-01 15:35:47 +0900

    Implemented gettimeofday() and to use it in the initialization process of e1000/ixgbe drivers to solve issue #43. (#44)
    * Implemented gettimeofday() system call
    * Implemented time stamp counter (TSC) synchronization between the bootstrap processor and application processors
    * Load the date and time from CMOS clock at the boot sequence
    * Use gettimeofday() and busy-wait approach in the initialization process of e1000/ixgbe driver to solve the issue #43

---
+++ src/kernel/arch/x86_64/cmos.h
@@ -0,0 +1,47 @@
+/*_
+ * Copyright (c) 2016 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _KERNEL_CMOS_H
+#define _KERNEL_CMOS_H
+
+#include <aos/const.h>
+#include <aos/types.h>
+#include "const.h"
+#include "../../kernel.h"
+
+#define MIN_YEAR        2016
+#define CMOS_ADDR       0x70
+#define CMOS_DATA       0x71
+
+u64 cmos_rtc_read_datetime(u8);
+
+#endif /* _KERNEL_CMOS_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
